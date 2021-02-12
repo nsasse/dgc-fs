1b6f11f1bdc0923e41c297dd7797f892b8d0346a
nsasse <nico.sasse@cofinpro.de>
2021-02-13 00:00:58 +0100

    init javax

---
+++ src/main/java/de/nsasse/dgc_fs/DgcApplication.java
@@ -0,0 +1,9 @@
+package de.nsasse.dgc_fs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class DgcApplication extends Application {
+
+}
